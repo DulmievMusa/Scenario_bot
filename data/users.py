7962171a0d75257e9ad7151173e7176fcf8ac02e
Musa Dulmiev <musadulmiev@gmail.com>
2023-04-28 22:08:38 +0300

    requirements.txt added

--- data/users.py
+++ data/users.py
@@ -3,10 +3,9 @@ import sqlalchemy
 from .db_session import SqlAlchemyBase
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy_serializer import SerializerMixin
-from flask_login import UserMixin
 
 
-class User(SqlAlchemyBase, SerializerMixin, UserMixin):
+class User(SqlAlchemyBase, SerializerMixin):
     __tablename__ = 'users'
 
     id = sqlalchemy.Column(sqlalchemy.Integer,
